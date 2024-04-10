4ac278df4c3d72f9694caf65725035fcff4ef0a7
KyberCritter <88512946+KyberCritter@users.noreply.github.com>
2024-04-10 11:52:25 -0500

    I hate GitHub Actions

--- build-docker-image.bat
+++ build-docker-image.bat
@@ -5,6 +5,6 @@ setlocal
 for /F "tokens=2 delims==" %%i in ('findstr GITHUB_TOKEN .env') do set GITHUB_TOKEN=%%i
 
 :: Build the Docker image
-docker build --build-arg GITHUB_TOKEN=%GITHUB_TOKEN% -t pygskin-webapp .
+docker build --build-arg GITHUB_TOKEN=%GITHUB_TOKEN% -t pygskin-webapp . --progress=plain --no-cache
 
 endlocal
