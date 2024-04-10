dd8b9b8316f4a1ab71f8b58cc27a4b346f3a8941
KyberCritter <88512946+KyberCritter@users.noreply.github.com>
2024-04-09 22:18:28 -0500

    added pie chart to stats and improved CSS

--- myproject/pygskin_webapp/views.py
+++ myproject/pygskin_webapp/views.py
@@ -35,6 +35,58 @@ cybercoach_paths = {
     }
 }
 
+def coach_list(request):
+    # Cybercoach access page
+    template = loader.get_template("pygskin_webapp/coach_list.html")
+    context = {
+        "cybercoach_paths": cybercoach_paths
+    }
+    return HttpResponse(template.render(context, request))
+
+def coach(request):
+    # Only proceed if this is a POST request
+    if request.method == 'POST':
+        # Extract the selected coach and model type from the POST request
+        selected_coach = request.POST.get('coach')
+
+        # Construct the cybercoach_path based on the selections
+        # This part assumes you have a way to determine the path from the selected coach and model type
+        if selected_coach in cybercoach_paths:
+            cybercoach_path = cybercoach_paths[selected_coach][[x for x in cybercoach_paths[selected_coach].keys()][0]]
+        else:
+            # Handle the case where the path does not exist for the selected options
+            return HttpResponse("Selected model path does not exist.", status=404)
+
+        # Load the cybercoach from the path
+        try:
+            cybercoach = pickle.load(open(cybercoach_path, "rb"))
+        except Exception as e:
+            return HttpResponse(f"Error loading model: {e}", status=500)
+        
+        # Gather playcalling statistics
+        play_dist = [value for value in cybercoach.play_distribution.values()]
+        play_types = [pygskin.PlayType(key).name for key in cybercoach.play_distribution.keys()]
+        colors = [pygskin.PLAY_TYPE_COLOR_DICT[pygskin.PlayType(key)] for key in cybercoach.play_distribution.keys()]
+
+        # Prepare context data for rendering
+        context = {
+            "coach_name": cybercoach.coach.coach_dict["first_name"] + " " + cybercoach.coach.coach_dict["last_name"],
+            "first_year": cybercoach.coach.first_year,
+            "last_year": cybercoach.coach.last_year,
+            "coach_seasons": cybercoach.coach.coach_dict["seasons"],
+            "play_dist": play_dist,
+            "play_types": play_types,
+            "colors": colors,
+        }
+
+        # Render and return the template with context
+        return render(request, "pygskin_webapp/coach.html", context)
+
+    else:
+        # Redirect or show an error for non-POST requests
+        # Adjust the redirect path as necessary
+        return redirect('index')
+
 def cybercoach_list(request):
     # Cybercoach access page
     template = loader.get_template("pygskin_webapp/cybercoach_list.html")
@@ -67,54 +119,13 @@ def cybercoach(request):
         # Prepare context data for rendering
         context = {
             "coach_name": cybercoach.coach.coach_dict["first_name"] + " " + cybercoach.coach.coach_dict["last_name"],
+            "first_year": cybercoach.coach.first_year,
+            "last_year": cybercoach.coach.last_year,
             "coach_seasons": cybercoach.coach.coach_dict["seasons"],
         }
 
         # Render and return the template with context
         return render(request, "pygskin_webapp/cybercoach.html", context)
-
-    else:
-        # Redirect or show an error for non-POST requests
-        # Adjust the redirect path as necessary
-        return redirect('index')
-
-def coach_list(request):
-    # Cybercoach access page
-    template = loader.get_template("pygskin_webapp/coach_list.html")
-    context = {
-        "cybercoach_paths": cybercoach_paths
-    }
-    return HttpResponse(template.render(context, request))
-
-def coach(request):
-    # Only proceed if this is a POST request
-    if request.method == 'POST':
-        # Extract the selected coach and model type from the POST request
-        selected_coach = request.POST.get('coach')
-        selected_model_type = request.POST.get('model_type')
-
-        # Construct the cybercoach_path based on the selections
-        # This part assumes you have a way to determine the path from the selected coach and model type
-        if selected_coach in cybercoach_paths and selected_model_type in cybercoach_paths[selected_coach]:
-            cybercoach_path = cybercoach_paths[selected_coach][selected_model_type]
-        else:
-            # Handle the case where the path does not exist for the selected options
-            return HttpResponse("Selected model path does not exist.", status=404)
-
-        # Load the cybercoach from the path
-        try:
-            cybercoach = pickle.load(open(cybercoach_path, "rb"))
-        except Exception as e:
-            return HttpResponse(f"Error loading model: {e}", status=500)
-
-        # Prepare context data for rendering
-        context = {
-            "coach_name": cybercoach.coach.coach_dict["first_name"] + " " + cybercoach.coach.coach_dict["last_name"],
-            "coach_seasons": cybercoach.coach.coach_dict["seasons"],
-        }
-
-        # Render and return the template with context
-        return render(request, "pygskin_webapp/coach.html", context)
 
     else:
         # Redirect or show an error for non-POST requests
