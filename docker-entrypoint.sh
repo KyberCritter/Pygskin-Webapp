b4d1b3685bb1d03d9153cc934366ada0807d998c
KyberCritter <88512946+KyberCritter@users.noreply.github.com>
2024-04-15 20:31:52 -0500

    load db dump on compose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,19 +2,32 @@
 
 echo "${RTE} runtime environment, running entrypoint"
 if [ "$RTE" = "dev" ]; then
-    echo "Making and applying Django migrations..."
+    echo "Flushing..."
     python myproject/manage.py flush --noinput
+    echo "Making and applying Django migrations..."
     python myproject/manage.py makemigrations --noinput
     python myproject/manage.py migrate --noinput
+    echo "Creating superuser"
     python myproject/manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+    echo "Loading database dump"
+    python myproject/manage.py loaddata data_dump.json
 elif [ "$RTE" = "setup" ]; then
     echo "Making and applying Django migrations..."
     python myproject/manage.py makemigrations
     python myproject/manage.py migrate --noinput
-    python myproject/manage.py createsuperuser --noinput
+    echo "Creating superuser"
+    python myproject/manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+    echo "Loading database dump"
+    python myproject/manage.py loaddata data_dump.json
 elif [ "$RTE" = "prod" ]; then
-    python manage.py check --deploy
-    # python manage.py collectstatic --noinput
+    echo "Making and applying Django migrations..."
+    python myproject/manage.py makemigrations
+    python myproject/manage.py migrate --noinput
+    echo "Creating superuser"
+    python myproject/manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+    echo "Loading database dump"
+    python myproject/manage.py loaddata data_dump.json
+    # python myproject/manage.py collectstatic --noinput
 fi
 
 echo "Starting server..."
