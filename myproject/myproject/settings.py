9d9ead14e4485413d64894d32d53519f59d41971
Riley Durbin <radurbin@crimson.ua.edu>
2024-10-01 14:41:17 -0500

    + changed to .upper()

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == "TRUE"
+DEBUG = os.getenv("DEBUG").upper() == "TRUE"
 
 # Cybercoach loading
 PATH_TO_CYBERCOACHES = "/app/myproject/pygskin_webapp/cybercoaches" if os.getenv("RUNNING_ON") == "DOCKER" else "./myproject/pygskin_webapp/cybercoaches"
