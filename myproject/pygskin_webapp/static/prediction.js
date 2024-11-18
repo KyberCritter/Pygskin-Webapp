6d3ecdcd8d57ef6b3f0d70b4df3f6838fd773e34
Riley Durbin <radurbin@crimson.ua.edu>
2024-11-18 12:25:04 -0600

    + Added comments to HTML, CSS, and JS pages that I worked on

--- myproject/pygskin_webapp/static/prediction.js
+++ myproject/pygskin_webapp/static/prediction.js
@@ -1,10 +1,11 @@
-    document.addEventListener('DOMContentLoaded', function () {
-        const correctCells = document.querySelectorAll('.correct');
-        const incorrectCells = document.querySelectorAll('.incorrect');
+// When the document loads, calculate the number of correct and incorrect predictions to display on the page
+document.addEventListener('DOMContentLoaded', function () {
+    const correctCells = document.querySelectorAll('.correct');
+    const incorrectCells = document.querySelectorAll('.incorrect');
 
-        const correctCount = correctCells.length;
-        const incorrectCount = incorrectCells.length;
-        const totalCells = correctCount + incorrectCount;
+    const correctCount = correctCells.length;
+    const incorrectCount = incorrectCells.length;
+    const totalCells = correctCount + incorrectCount;
 
-        document.getElementById("prediction-stat").innerHTML = "THE MACHINE LEARNING MODEL CORRECTLY PREDICTED " + correctCount + "/" + totalCells + " PLAYS";
-    });
+    document.getElementById("prediction-stat").innerHTML = "THE MACHINE LEARNING MODEL CORRECTLY PREDICTED " + correctCount + "/" + totalCells + " PLAYS";
+});
